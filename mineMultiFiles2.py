df406eecd3e6abb270d70ef1d6310df240fb9250
daniel phillis <daniel.phillis@gmail.com>
2016-10-19 17:04:35 +1030

    Update mineMultiFiles2.py

--- mineMultiFiles2.py
+++ mineMultiFiles2.py
@@ -1,3 +1,32 @@
+#------------------------------------------------------------------
+# This code thankfully acknowledges the python from the book
+# "Mining the Social Web 2nd Edition
+# and the tutorials by Harrison on the youtube channel "sendex"
+
+# this code developed by daniel phillis
+# SID 2110633, FAN phil0411
+# with the help mentioned above
+#------------------------------------------------------------------
+#Summary
+# you can now specify a path as an argument
+# to pointthe sciript to a particular bunch of files
+# ie run_my_script.py --path ../../wierd_directory/my_wteet_data.txt
+# defualt path is set but can be edited
+# the script will LOOP OVER ALL text files in the target directory and parse them for keywords
+# which are hardcoded in this script
+# the file path can and currently being stripped off so that results are easily found
+# - they are written to files in the same directory as the script's location when run
+
+# only text files are parsed (with .txt extension) other files are ignored
+# see "### FILE EXTENSION FILTER"
+# result values are normalised over the total number of words parsed.
+
+# in consideration for next version
+# reading keywords off disk
+
+# problem$: clean function call is commented out needs to be fixed
+# adjective functionality currently commented out
+
 import sys
 import subprocess
 from subprocess import call
@@ -34,7 +63,7 @@ def analyse_content(data, keywords, size, entityThreshold=3):
 		return
 
 	stop_Words = stopwords.words("english")
-	#camel case convention is broken to accentuate custom var over nltk variable
+	#camel case convention is broken to accentuate custom var over nltk module 'stopwords'
 
 	customStopWords = [
 						"Galaxy","Samsung","Note","replies","retweets",
@@ -83,7 +112,6 @@ def analyse_content(data, keywords, size, entityThreshold=3):
 # clean ignores hashtags (#example) when building output list
 # clean ignores twitter usernames (@username) when building output list
 # clean also gives a before and after count of elements
-
 def clean(items):
 	start = time.time()
 	print("cleaning...")
@@ -155,13 +183,14 @@ def stripPath(fileName):
 	fileName = oldPath[pathSize-1]
 
 	#strip txt extension
-	fl = len(fileName)	#print(fileName[:fl-1-3])	
+	fl = len(fileName)	#print(fileName[:fl-1-3])
+	### FILE EXTENSION FILTER
 	if str(fileName[(fl-4):]) == '.txt':# 4 chars in ".txt"
 		fileName = fileName[:fl-4]#print("extension removed:" + fileName)
 	print("new fileName:" + fileName)
 	return fileName
 #--------------------------------------------------------------------------
-def writeCSVDict(fileName, tuples):
+def writeCSVDict(fileName, tuples, total):
 	print("writing csv...")	
 	#if file exits already, open and append vaues to correct keys
 	#add total count
@@ -173,20 +202,15 @@ def writeCSVDict(fileName, tuples):
 	fil = str(fileName) + ext
 	fileName += ext
 	#print len(tuples)
-	#read existing values to append to
-	'''
-	if io.open('{0}'.format(fileName),'a', encoding ='utf-8'):
-		with io.open('{0}'.format(fileName),'a', encoding ='utf-8') as f:
-			for i in f:
-				print i
-	'''
-	total = 0
-
+	mysum = 0
 	with io.open('{0}'.format(fileName),'a', encoding ='utf-8') as f:
 		for (k,v) in tuples.iteritems(): #
-			total += v
-			f.write(unicode(str(k) + "\t\t" + str(v) + "\n"))
-		#f.write(unicode("\ntotal specified keywords found:\t" + str(total) + "\n"))
+			#normalise values over total ords parsed
+			fv = float(v)/(float(total))
+			mysum += v
+			f.write(unicode(str(k) + "\t\t" + str(fv) + "\n"))
+		f.write(unicode("\ntotal specified keywords found:\t" + str(mysum) + "\n"))	
+		f.write(unicode("\ntotal words parsed:\t" + str(total) + "\n"))
 	f.close()
 	print("* WroteFile :" + fil)
 
@@ -234,6 +258,7 @@ keywords = {#dictionary
 #count = 100
 search_results = []
 size = 25
+total = 0
 v = 18 #optional version identifier suffix for fileName
 #path = "../output/leg/aug/samsung/"
 #path no longer hardcoded, but taken from Cmd Line args
@@ -255,14 +280,14 @@ for fileName in files:
 		tweetFile = open(str(f),'r').read().decode('utf-8')
 		#tweetFile = open(f,'r').read()
 		search_results = word_tokenize(tweetFile)
-		#print "length of results:",len(search_results)
+		total += len(search_results)
 
 		# additive process
 		keywords = analyse_content(search_results, keywords, size)
 		#(analyse_content prints a 	prettyTable)
 
 #returns a dictionary?/array of tuples for writing a FREQ DIST	
-writeCSVDict("merged", keywords) #file gets the query name
+writeCSVDict("merged", keywords, total) #file gets the query name, and total words parsed data
 #------------------------------------------------------------------        
 # timing - Ideally collect into an array of times and print at the end 
 # of a multi query
